f2e732129c8ad05d5c2e8caafd4f03dd59d04279
tjdock <565130824@qq.com>
2021-02-03 07:45:42 +0800

    add css

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,22 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
 let mode = 'development';
+let target = 'web';
+
 if(process.env.NODE_ENV === 'production'){
     mode = 'production';
+    target = 'browserslist';
 }
 
 module.exports={
     mode: mode,
+    target: target,
     module: {
         rules: [
+            {
+                test:/\.(s[ac]|c)ss$/i,
+                use:[MiniCssExtractPlugin.loader,"css-loader","postcss-loader","sass-loader"]
+            },
             {
                 test:/\.js$/,
                 exclude: /node_modules/,
@@ -14,8 +24,10 @@ module.exports={
             }
         ]
     },
+    plugins: [new MiniCssExtractPlugin()],
     devtool: 'source-map',
     devServer: {
-        contentBase:'./dist'
+        contentBase:'./dist',
+        hot:true,
     }
 }
