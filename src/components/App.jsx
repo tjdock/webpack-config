0c1cbe8a7a34f188de9cf3092052bfa4fbdd896b
tjdock <565130824@qq.com>
2021-02-03 08:45:16 +0800

    add react

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import '../styles/index.scss';
+import Recipes from "./Recipes";
+const App = () => {
+    return (
+        <>
+            <section className='hero'></section>
+            <main>
+                <section>
+                    <h2>Oh Hai,React</h2>
+                </section>
+            </main>
+
+            <Recipes/>
+        </>
+    );
+}
+
+export default App;
